d1f9f1e62fab37ba27bcf1ccea466f801c8f04eb
dathompson <da@dathompson.com>
2012-02-09 15:00:52 -0800

    added mingw to windows os

--- lib/wkhtmltopdfify.rb
+++ lib/wkhtmltopdfify.rb
@@ -1,21 +1,16 @@
-require 'wkhtmltopdfify/version'
-require 'rbconfig'
+require "wkhtmltopdfify/version"
 
 module Wkhtmltopdfify
-  
-  host_os = RbConfig::CONFIG['host_os']
-
-  def self.determine_os_and_executable(os_string)
-    case os_string
-      when /darwin/ then @executable = 'bin/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf'
-      when /linux/ then @executable = 'bin/linux/wkhtmltopdf_linux_386'
-      when /mswin|mingw/ then @executable = 'bin/windows/wkhtmltopdf.exe'
-      else raise InvalidOSError('This is not a supported Operating System. Please install wkhtmltopdf manually.')
-    end
-  
-    @executable = File.join(File.dirname(__FILE__), @executable)
-    system(@executable + " " + $*.join(" "))
+  if RUBY_PLATFORM =~ /linux/
+    executable = 'bin/linux/wkhtmltopdf_linux_386'
+  elsif RUBY_PLATFORM =~ /darwin/
+    executable = 'bin/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf'
+  elsif RUBY_PLATFORM =~ /mswin32/ or /mingw32/ or /mingw/
+    executable = 'bin/windows/wkhtmltopdf.exe'
+  else
+    raise "Something doesn't seem to be working. If you are running on Windows, you may need to install wkhtmltopdf manually. exe's available here: http://code.google.com/p/wkhtmltopdf/downloads/list Otherwise, you may need to install wkhtmltopdf manually. Try https://github.com/blueheadpublishing/bookshop/wiki/Installing-wkhtmltopdf"
   end
-  determine_os_and_executable(host_os)
-    
+  
+  executable = File.join(File.dirname(__FILE__), executable)
+  system(executable + " " + $*.join(" "))
 end
